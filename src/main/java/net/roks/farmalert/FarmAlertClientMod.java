991807ec07b6ef86dc97c7c27657faeb2ea6480a
roks <corokso11@gmail.com>
2026-07-12 16:52:06 +0530

    Milestone 1 - Display test title

--- src/main/java/net/roks/farmalert/FarmAlertClientMod.java
+++ src/main/java/net/roks/farmalert/FarmAlertClientMod.java
@@ -1,15 +1,35 @@
 package net.roks.farmalert;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 
 public class FarmAlertClientMod implements ClientModInitializer {
+
+    private int ticks = 0;
 
     @Override
     public void onInitializeClient() {
 
-        FarmAlert.LOGGER.info("==================================");
-        FarmAlert.LOGGER.info(" FarmAlert Client Loaded ");
-        FarmAlert.LOGGER.info("==================================");
+        FarmAlert.LOGGER.info("FarmAlert client loaded.");
 
+        ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
+
+            if (minecraft.player == null) {
+                return;
+            }
+
+            ticks++;
+
+            if (ticks >= 100) {
+
+                ticks = 0;
+
+                minecraft.gui.setTimes(5, 40, 10);
+                minecraft.gui.setTitle(Component.literal("TEST"));
+            }
+        });
     }
 }
